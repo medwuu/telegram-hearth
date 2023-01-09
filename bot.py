6d6525a988dad5b23d6136f63c09b4a2c525a422
medwuu <91782808+medwuu@users.noreply.github.com>
2023-01-10 00:13:26 +0300

    working

--- bot.py
+++ bot.py
@@ -1,10 +1,14 @@
+# todo:
+# сделать inline mode
+
 import telebot
-import config
 import time
 
-bot = telebot.TeleBot(config.TOKEN)
+# вставьте свой токен ниже
+TOKEN = ""
+bot = telebot.TeleBot(TOKEN)
 
-basic_hearth = "\
+hearth = "\
 🤍️🤍️🤍️🤍️🤍️🤍️🤍️🤍️🤍️\n\
 🤍️🤍️❤️❤️🤍️❤️❤️🤍️🤍️\n\
 🤍️❤️❤️❤️❤️❤️❤️❤️🤍️\n\
@@ -15,34 +19,78 @@ basic_hearth = "\
 🤍️🤍️🤍️🤍️❤️🤍️🤍️🤍️🤍️\n\
 🤍️🤍️🤍️🤍️🤍️🤍️🤍️🤍️🤍️"
 
+circle = "\
+⚪⚪⚪⚪⚪⚪⚪⚪⚪\n\
+⚪⚪⚪🔴🔴🔴⚪⚪⚪\n\
+⚪⚪🔴🔴🔴🔴🔴⚪⚪\n\
+⚪🔴🔴🔴🔴🔴🔴🔴⚪\n\
+⚪🔴🔴🔴🔴🔴🔴🔴⚪\n\
+⚪🔴🔴🔴🔴🔴🔴🔴⚪\n\
+⚪⚪🔴🔴🔴🔴🔴⚪⚪\n\
+⚪⚪⚪🔴🔴🔴⚪⚪⚪\n\
+⚪⚪⚪⚪⚪⚪⚪⚪⚪"
+
+square = "\
+⬜⬜⬜⬜⬜⬜⬜⬜⬜\n\
+⬜🟥🟥🟥🟥🟥🟥🟥⬜\n\
+⬜🟥🟥🟥🟥🟥🟥🟥⬜\n\
+⬜🟥🟥🟥🟥🟥🟥🟥⬜\n\
+⬜🟥🟥🟥🟥🟥🟥🟥⬜\n\
+⬜🟥🟥🟥🟥🟥🟥🟥⬜\n\
+⬜🟥🟥🟥🟥🟥🟥🟥⬜\n\
+⬜🟥🟥🟥🟥🟥🟥🟥⬜\n\
+⬜⬜⬜⬜⬜⬜⬜⬜⬜"
+
+
+@bot.message_handler(commands=['start', 'help'])
+def start(message):
+    bot.send_video(message.chat.id, open("media/presentation.mp4", "rb"), caption="Привет, я бот, который делает ✨магию✨. Просто отправь мне название фигуры (сердце, квадрат, или круг), а затем любой текст и увидишь магию!\nПример:\n<code>круг Привет, Мир!</code>\n<code>Сердце Люблю Вас!</code>", parse_mode='html')
+
+
 
 @bot.message_handler(content_types=['text'])
-def hearth(message):
-    message_id = bot.send_message(message.chat.id, basic_hearth)
+def beautiful_hearth(message):
+    # определение фигуры
+    try:
+        figure = {"сердце" : hearth, "круг" : circle, "квадрат" : square}[message.text.split(" ", 1)[0].lower()]
+        print(message.text.lower().split(" ", 1))
+    except KeyError:
+        bot.send_message(message.chat.id, "Упс, что-то пошло не так. Проверьте формат сообщения!")
+        return "Invalid Input"
+    # установка подходящего фигуре эмодзи
+    if figure == hearth:
+        red, orange, yellow, blue, green, white = ["❤️", "🧡", "💛", "💙", "💚", "🤍"]
+    elif figure == circle:
+        red, orange, yellow, blue, green, white = ["🔴", "🟠", "🟡", "🔵", "🟢", "⚪"]
+    elif figure == square:
+        red, orange, yellow, blue, green, white = ["🟥", "🟧", "🟨", "🟦", "🟩", "⬜"]
+    message_id = bot.send_message(message.chat.id, figure).message_id
     time.sleep(0.5)
-    bot.edit_message_text(basic_hearth.replace("❤️", "🧡"), message.chat.id, message_id.message_id)
+    bot.edit_message_text(figure.replace(red, orange), message.chat.id, message_id)
     time.sleep(0.5)
-    bot.edit_message_text(basic_hearth.replace("❤️", "💛"), message.chat.id, message_id.message_id)
+    bot.edit_message_text(figure.replace(red, yellow), message.chat.id, message_id)
     time.sleep(0.5)
-    bot.edit_message_text(basic_hearth.replace("❤️", "💙"), message.chat.id, message_id.message_id)
+    bot.edit_message_text(figure.replace(red, blue), message.chat.id, message_id)
     time.sleep(0.5)
-    bot.edit_message_text(basic_hearth.replace("❤️", "💚"), message.chat.id, message_id.message_id).text
-    basic_hearth1 = basic_hearth.replace("❤️", "💚")
+    final_figure = bot.edit_message_text(figure.replace(red, green), message.chat.id, message_id).text
+    # замена надписи на фон
     counter = 1
-    for i in basic_hearth1:
-        if i == "💚":
-            bot.edit_message_text(basic_hearth1.replace(i, "🤍", counter), message.chat.id, message_id.message_id)
+    for is_green in final_figure:
+        if is_green == green:
+            bot.edit_message_text(final_figure.replace(is_green, white, counter), message.chat.id, message_id)
             counter += 1
             time.sleep(0.01)
-
+    # постепенное исчезновение
     for cycle in range(8, 0, -1):
         string = ""
         for vert in range(cycle, 0, -1):
             for horiz in range(cycle, 0, -1):
-                string += "🤍"
+                string += white
             string += "\n"
-        bot.edit_message_text(string, message.chat.id, message_id.message_id)
-    bot.edit_message_text(message.text, message.chat.id, message_id.message_id)
+        bot.edit_message_text(string, message.chat.id, message_id)
+        time.sleep(0.2)
+    # вывод текста пользователя
+    bot.edit_message_text(message.text.split(" ", 1)[1], message.chat.id, message_id)
 
 
 
